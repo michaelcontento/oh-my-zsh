5041fa0bb8e3488d826cd762eedcafc13b4f333c
Michael Contento <michaelcontento@gmail.com>
2012-04-02 09:41:50 +0200

    OSX bindkey fixed as own plugin

---
+++ custom/plugins/osx-bindkey/osx-bindkey.plugin.zsh
@@ -0,0 +1,7 @@
+# Fix alt-left and alt-right in Terminal.app
+bindkey "\e[1;9D" backward-word
+bindkey "\e[1;9C" forward-word
+
+# Fix alt-left and alt-right in tmux
+bindkey "\e[1;3D" backward-word
+bindkey "\e[1;3C" forward-word
