04553c68881013bae74a8552a22e076eb2f10bd3
Michael Contento <michaelcontento@gmail.com>
2012-04-10 10:17:50 +0200

    alt-(left|right) fixed in iTerm2

--- custom/plugins/osx-bindkey/osx-bindkey.plugin.zsh
+++ custom/plugins/osx-bindkey/osx-bindkey.plugin.zsh
@@ -5,3 +5,7 @@ bindkey "\e[1;9C" forward-word
 # Fix alt-left and alt-right in tmux
 bindkey "\e[1;3D" backward-word
 bindkey "\e[1;3C" forward-word
+
+# Fix alt-left and alt-right in iTerm2
+bindkey "\e\e[C" forward-word
+bindkey "\e\e[D" backward-word
