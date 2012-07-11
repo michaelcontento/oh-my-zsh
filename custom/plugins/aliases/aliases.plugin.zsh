f2241e0dfa1e1dd57d08bb60c95a8d26312be83b
Michael Contento <michaelcontento@gmail.com>
2012-07-11 19:30:27 +0200

    Alias l and ll now OSX compatible

--- custom/plugins/aliases/aliases.plugin.zsh
+++ custom/plugins/aliases/aliases.plugin.zsh
@@ -1,4 +1,12 @@
-alias l='ls --color=auto -lh'
-alias la='ls --color=auto -lhA'
+if [ "$(uname)" = "Darwin" ]; then
+    COLOR="-G"
+else
+    COLOR="--color=auto"
+fi
+
+alias l="ls $COLOR -lh"
+alias ll="ls $COLOR -lhA"
 
 alias json_pp='python -mjson.tool'
+
+unset COLOR
